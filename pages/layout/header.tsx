e1699d351c0ccf41bb8ede31009c1b9d48e955dd
Riajul Islam <javascriptforeverything@gmail.com>
2023-01-27 21:18:32 +0600

    add menu icon on mobile view and nav bar in desktop view

--- pages/layout/header.tsx
+++ pages/layout/header.tsx
@@ -1,16 +1,20 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 
-import Button from '@mui/material/Button'
-import Box from '@mui/material/Box'
-import Container from '@mui/material/Container'
 import { useTheme } from '@mui/material/styles'
+import Container from '@mui/material/Container'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import IconButton from '@mui/material/IconButton'
+
+import MenuIcon from '@mui/icons-material/Menu'
+import Avatar from '@mui/material/Avatar'
 
 const navItems = [
 	{ label: 'home', path: '/' },
 	{ label: 'about', path: '/about' },
 	{ label: 'projects', path: '/project' },
-	{ label: 'contact me', path: '/contact' },
+	// { label: 'contact me', path: '/contact' },
 ]
 
 
@@ -22,23 +26,44 @@ const Header = () => {
 	console.log(router.pathname)
 	
 	return (
-		<Box sx={ theme => ({
-			pt: 1,
-			borderBottom: `1px solid ${theme.palette.divider}`
-		})}>
+		<Box sx={ theme => ({ pt: 1, borderBottom: `1px solid ${theme.palette.divider}` })}>
 			<Container>
-				<Box sx={{ display: 'flex', alignItems: 'center' }} >
-				{navItems.map(({ label, path }, index, arr) => (
-					<Link key={label} href={path} passHref style={{
-						marginLeft: index + 1 === arr.length ? 'auto' : 0,
 
-						paddingBottom: 8,
-						borderBottom: router.pathname === path ?  `1px solid ${theme.palette.primary.main}` : '1px solid transparent'
-					}}>
-						<Button variant={ index + 1 === navItems.length ? 'outlined' : 'text' }>{label}</Button>
-					</Link>
-				))}
+				<Box sx={{ display: { xs: 'block', sm: 'none' }}}>
+					<Box sx={{ display: 'flex', alignItems: 'center' }}>
+						<Box>
+							<IconButton>
+								<MenuIcon />
+							</IconButton>
+						</Box>
+
+						<Link href={'/contact'} passHref style={{ marginLeft: 'auto' }}>
+							<IconButton size='small'>
+								<Avatar src='/images/aboutAvatar.png' />
+							</IconButton>
+						</Link>
+
+					</Box>
 				</Box>
+
+				<Box sx={{ display: { xs: 'none', sm: 'block' }}}>
+					<Box sx={{ display: 'flex', alignItems: 'center' }} >
+						{navItems.map(({ label, path }) => (
+							<Link key={label} href={path} passHref style={{
+								paddingBottom: 8,
+								borderBottom: router.pathname === path ?  `1px solid ${theme.palette.primary.main}` : '1px solid transparent'
+							}}>
+								<Button>{label}</Button>
+							</Link>
+						))}
+						<Link href='/contact' passHref style={{ marginLeft: 'auto' }}>
+							<IconButton size='small'>
+								<Avatar src='/images/aboutAvatar.png' />
+							</IconButton>
+						</Link>
+					</Box>
+				</Box>
+
 			</Container>
 		</Box>
 	)
